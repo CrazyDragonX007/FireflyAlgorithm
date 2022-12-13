36311649287ad3f247b348c8bb23c762498d6119
viper-vm <45145119+viper-vm@users.noreply.github.com>
2022-12-13 03:15:13 -0500

    Update main.py

--- main.py
+++ main.py
@@ -3,10 +3,8 @@ from swarmlib import FUNCTIONS
 from firefly import Firefly
 FA = Firefly()
 bestvals,best = FA.run(function=FUNCTIONS['rastrigin'], dim=1, lowerbound=-5.12, upperbound=5.12, max_evals=10000, pop_size=100)
-lenbestval = len(bestvals)
-arr = []
-for i in range(0,lenbestval):
-    arr.append(i)
+
+
 plt.plot(bestvals)
 plt.title("Function 1")
 plt.xlabel("Iterations")
